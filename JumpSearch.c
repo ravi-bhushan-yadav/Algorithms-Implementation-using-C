fd93c5c40b76af3dbe3e1a1cdbc4fe5ae3a897a9
Ravi Bhushan Kumar <rbhushan10401@gmail.com>
2025-12-10 16:12:06 +0530

    Create JumpSearch.c

---
+++ JumpSearch.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+#include<math.h>
+
+int jump_search(int[], int, int);
+
+int main(){
+   int i, n, key, index;
+   int arr[12] = {0, 6, 12, 14, 19, 22, 48, 66, 79, 88, 104, 126};
+   int len = sizeof(arr)/sizeof(arr[0]);
+   printf("Array elements are:");
+   for(int j = 0; j<len; j++){
+      printf(" %d", arr[j]);
+   }
+   n = 12;
+   key = 66;
+   printf("\nThe element to be searched: %d\n", key);
+   index = jump_search(arr, n, key);
+   if(index >= 0)
+      printf("The element is found at position %d", index+1);
+   else
+      printf("Unsuccessful Search");
+   return 0;
+}
+int jump_search(int arr[], int n, int key){
+   int i, j, m, k;
+   i = 0;
+   m = sqrt(n);
+   k = m;
+   while(arr[m] <= key && m < n) {
+      i = m;
+      m += k;
+      if(m > n - 1)
+         return -1;
+   }
+
+   // linear search on the block
+   for(j = i; j<m; j++) {
+      if(arr[j] == key)
+         return j;
+   }
+   return -1;
+}
